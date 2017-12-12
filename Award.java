12aa22fa0e3fee454bf0f37128e37eb7a039397e
(Eva) Yiwen Luo <yiwen@brandeis.edu>
2017-12-12 14:58:54 -0500

    Create Award.java

---
+++ Award.java
@@ -0,0 +1,9 @@
+package plane;
+
+public interface Award {
+	public static final int DOUBLE_FIRE = 0;   //int DOUBLE_FIRE = 0;
+	public static final int LIFE = 1;   //int LIFE = 1;
+
+	public int getType();
+
+}
